f414714676f64ee30bf239fcb9bccb3f93bb778a
Fredrik Estelius <fredrik.estelius@gmail.com>
2015-11-22 22:22:44 +0100

    Initializes basic sdl window and event systems and introduces the basic game loop.

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,28 @@
 extern crate sdl2;
 mod asteroids;
+use sdl2::event::Event;
 
 fn main() {
-    println!("Hello, world!");
-    asteroids::update_and_render();
+    let context = sdl2::init()
+        .expect("Could not initialize SDL2.");
+    let mut events = context.event_pump()
+        .expect("Could not create SDL2 event pump.");
+    let video = context.video()
+        .expect("Could not create SDL2 video subsystem.");
+    let window = video.window("Asteroids", 800, 600)
+        .position_centered()
+        .build()
+        .expect("Could not build SDL2 window.");
+
+    'game: loop {
+        for event in events.poll_iter() {
+            match event {
+                Event::Quit {..} => break 'game,
+                Event::KeyDown {..} => break 'game,
+                _ => ()
+            }
+        }
+        asteroids::update_and_render();
+        std::thread::sleep_ms(100);
+    }
 }
