f9003410a808bf4f8aea2b574b0269c7bb89036c
Fredrik Estelius <fredrik.estelius@gmail.com>
2015-11-22 21:59:02 +0100

    Adds area for platform independent code.

---
+++ src/asteroids.rs
@@ -0,0 +1,4 @@
+pub fn update_and_render() {
+    println!("Update");
+    println!("Render");
+}
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
 extern crate sdl2;
+mod asteroids;
 
 fn main() {
     println!("Hello, world!");
+    asteroids::update_and_render();
 }
