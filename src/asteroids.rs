366b54d724fc4a7304dcf730148735ce06250724
Fredrik Estelius <fredrik.estelius@gmail.com>
2015-11-22 22:59:09 +0100

    Adds passing of input from platform to game in a crude manner. Also introduces an Asteroids object to keep track of state.

--- src/asteroids.rs
+++ src/asteroids.rs
@@ -1,4 +1,24 @@
-pub fn update_and_render() {
+pub struct Asteroids {
+    should_continue: bool,
+}
+
+impl Asteroids {
+    pub fn new() -> Asteroids {
+        Asteroids {
+            should_continue: true,
+        }
+    }
+
+    pub fn should_continue(&self) -> bool {
+        self.should_continue
+    }
+}
+
+pub fn update_and_render(asteroids: &mut Asteroids, input: &[char]) {
+    if input.iter().any(|&i| i == 'q') {
+        asteroids.should_continue = false;
+        return
+    }
     println!("Update");
     println!("Render");
 }
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,9 @@
 extern crate sdl2;
+
 mod asteroids;
+
 use sdl2::event::Event;
+use sdl2::keyboard::Scancode;
 
 fn main() {
     let context = sdl2::init()
@@ -14,15 +17,21 @@ fn main() {
         .build()
         .expect("Could not build SDL2 window.");
 
-    'game: loop {
-        for event in events.poll_iter() {
-            match event {
-                Event::Quit {..} => break 'game,
-                Event::KeyDown {..} => break 'game,
-                _ => ()
-            }
-        }
-        asteroids::update_and_render();
+    let mut asteroids = asteroids::Asteroids::new();
+
+    while asteroids.should_continue() {
+        let input = events
+            .poll_iter()
+            .map(|e| translate_sdl2_event(e))
+            .collect::<Vec<char>>();
+        asteroids::update_and_render(&mut asteroids, &input);
         std::thread::sleep_ms(100);
+    }
+}
+
+fn translate_sdl2_event(event: Event) -> char {
+    match event {
+        Event::KeyDown {scancode: Some(Scancode::Q), ..} => 'q',
+        _ => ' ',
     }
 }
