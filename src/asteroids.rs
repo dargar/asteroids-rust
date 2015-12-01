3fa5234b86bd03b011e3c29e7ea20d024d4481d6
Fredrik Estelius <fredrik.estelius@gmail.com>
2015-12-01 09:19:00 +0100

    Adds component system to entity. Adds a few more entities and the capability to update and render more than one entity.

--- src/asteroids.rs
+++ src/asteroids.rs
@@ -6,23 +6,28 @@ use cgmath::Matrix;
 use cgmath::SquareMatrix;
 use cgmath::Vector4;
 use cgmath::Vector;
+use super::entity::Component;
 use super::entity::Entity;
+use super::entity::EntityState;
 use super::render;
 
 pub struct Asteroids {
     should_continue: bool,
     vao: u32,
     projection: Matrix4<f32>,
-    entity: Entity,
+    entities: Vec<Entity>,
+    state: EntityState,
 }
 
 impl Asteroids {
     pub fn new() -> Asteroids {
+        let entity_state = EntityState::new();
         Asteroids {
             should_continue: true,
             vao: 0,
             projection: cgmath::ortho(0.0, 800.0, 600.0, 0.0, -1.0, 1.0),
-            entity: Entity::new(0, Vector4::new(400.0, 300.0, 0.0, 1.0)),
+            entities: Vec::new(),
+            state: entity_state,
         }
     }
 
@@ -36,49 +41,66 @@ pub fn update_and_render(asteroids: &mut Asteroids, input: &[char]) {
         asteroids.vao = render::create_object(0);
     }
 
-    let mut a: Vector4<f32> = Vector4::zero();
-    for &event in input {
-        match event {
-            'q' => {
-                asteroids.should_continue = false;
-                return
+    if asteroids.entities.is_empty() {
+        asteroids.entities.push(Entity::player_ship(&mut asteroids.state));
+        asteroids.entities.push(Entity::asteroid(&mut asteroids.state));
+        asteroids.entities.push(Entity::asteroid(&mut asteroids.state));
+        asteroids.entities.push(Entity::asteroid(&mut asteroids.state));
+    }
+
+    {
+        let ref mut direction = asteroids.state.directions[*asteroids.entities[0].components.get(&Component::Direction).unwrap()];
+        let ref mut acceleration = asteroids.state.accelerations[*asteroids.entities[0].components.get(&Component::Acceleration).unwrap()];
+        for &event in input {
+            match event {
+                'w' => {
+                    acceleration.x += cgmath::sin(cgmath::deg(*direction));
+                    acceleration.y += -cgmath::cos(cgmath::deg(*direction));
+                },
+                'a' => *direction += -4.0,
+                'd' => *direction += 4.0,
+                'q' => {
+                    asteroids.should_continue = false;
+                    return
+                }
+                _ => (),
             }
-            'w' => {
-                a.x += cgmath::sin(cgmath::deg(asteroids.entity.direction));
-                a.y += -cgmath::cos(cgmath::deg(asteroids.entity.direction));
-            },
-            'a' => asteroids.entity.direction += -4.0,
-            'd' => asteroids.entity.direction += 4.0,
-            _ => (),
         }
     }
 
-    asteroids.entity.acceleration = a;
-    asteroids.entity.update(1.0 / 60.0);
-
-    let mut model = Matrix4::one();
-
-    let mut translation = Matrix4::one();
-    translation.replace_col(3, asteroids.entity.position);
-    model = model.mul_m(&translation);
-    
-    let mut rotation_z = Matrix4::one();
-    let theta = asteroids.entity.direction;
-    rotation_z[0][0] = cgmath::cos(cgmath::deg(theta));
-    rotation_z[0][1] = cgmath::sin(cgmath::deg(theta));
-    rotation_z[1][0] = -cgmath::sin(cgmath::deg(theta));
-    rotation_z[1][1] = cgmath::cos(cgmath::deg(theta));
-    model = model.mul_m(&rotation_z);
-
-    let scaling = Matrix4::from_diagonal(cgmath::Vector4::new(25.0f32, 50.0f32, 0.0f32, 1.0f32));
-    model = model.mul_m(&scaling);
-
-    let mvp = asteroids.projection.mul_m(&model);
-    let mvp_array: [f32; 16] = *mvp.as_ref();
+    for entity in &asteroids.entities {
+        entity.update(&mut asteroids.state, 1.0 / 60.0);
+    }
 
     unsafe {
         gl::Clear(gl::COLOR_BUFFER_BIT);
-        gl::UniformMatrix4fv(1, 1, gl::FALSE, mvp_array.as_ptr());
-        gl::DrawArrays(gl::LINE_LOOP, 0, 3);
+    }
+
+    for entity in &asteroids.entities {
+        let mut model = Matrix4::one();
+
+        let mut translation = Matrix4::one();
+        let position = asteroids.state.positions[*entity.components.get(&Component::Position).unwrap()];
+        translation.replace_col(3, position);
+        model = model.mul_m(&translation);
+
+        let mut rotation_z = Matrix4::one();
+        let theta = asteroids.state.directions[*entity.components.get(&Component::Direction).unwrap()];
+        rotation_z[0][0] = cgmath::cos(cgmath::deg(theta));
+        rotation_z[0][1] = cgmath::sin(cgmath::deg(theta));
+        rotation_z[1][0] = -cgmath::sin(cgmath::deg(theta));
+        rotation_z[1][1] = cgmath::cos(cgmath::deg(theta));
+        model = model.mul_m(&rotation_z);
+
+        let scaling = Matrix4::from_diagonal(Vector4::new(25.0f32, 50.0f32, 0.0f32, 1.0f32));
+        model = model.mul_m(&scaling);
+
+        let mvp = asteroids.projection.mul_m(&model);
+        let mvp_array: [f32; 16] = *mvp.as_ref();
+
+        unsafe {
+            gl::UniformMatrix4fv(1, 1, gl::FALSE, mvp_array.as_ptr());
+            gl::DrawArrays(gl::LINE_LOOP, 0, 3);
+        }
     }
 }
--- src/entity.rs
+++ src/entity.rs
@@ -1,44 +1,151 @@
 extern crate cgmath;
+extern crate rand;
 
 use cgmath::Vector4;
 use cgmath::Vector;
+use self::rand::Rng;
+use std::collections::HashMap;
+
+#[derive(Eq, PartialEq, Hash)]
+pub enum Component {
+    Acceleration,
+    Position,
+    Velocity,
+    Direction,
+}
 
 pub struct Entity {
     pub id: u32,
-    pub position: Vector4<f32>,
-    pub velocity: Vector4<f32>,
-    pub acceleration: Vector4<f32>,
-    pub direction: f32,
+    pub components: HashMap<Component, usize>,
 }
 
 impl Entity {
-    pub fn new(id: u32, position: Vector4<f32>) -> Entity {
+    fn new(id: u32) -> Entity {
         Entity {
             id: id,
-            position: position,
-            velocity: Vector4::zero(),
-            acceleration: Vector4::zero(),
-            direction: 0.0,
+            components: HashMap::new(),
         }
     }
 
-    pub fn update(&mut self, t: f32) {
-        self.acceleration = self.acceleration * 1000.0;
-        self.position = self.acceleration * t * t * 0.5f32 + self.velocity * t + self.position;
-        self.velocity = self.acceleration * t + self.velocity;
+    pub fn player_ship(state: &mut EntityState) -> Entity {
+        let mut entity = Entity::new(state.next_id());
 
-        if self.position.x < 0.0 {
-            self.position.x = 800.0;
-        } else if self.position.x > 800.0 {
-            self.position.x = 0.0;
+        let acceleration = state.add_acceleration(Vector4::zero());
+        entity.components.insert(Component::Acceleration, acceleration);
+
+        let position = state.add_position(Vector4::new(400.0, 300.0, 0.0, 1.0));
+        entity.components.insert(Component::Position, position);
+
+        let velocity = state.add_velocity(Vector4::zero());
+        entity.components.insert(Component::Velocity, velocity);
+
+        let direction = state.add_direction(0.0);
+        entity.components.insert(Component::Direction, direction);
+
+        entity
+    }
+
+    pub fn asteroid(state: &mut EntityState) -> Entity {
+        let mut entity = Entity::new(state.next_id());
+
+        let mut rng = rand::StdRng::new()
+            .expect("Could not load random number generator.");
+
+        let px = rng.next_f32() * 800.0;
+        let py = rng.next_f32() * 600.0;
+        let position = state.add_position(Vector4::new(px, py, 0.0, 1.0));
+        entity.components.insert(Component::Position, position);
+
+        let dir = rng.next_f32() * 360.0;
+        let direction = state.add_direction(dir);
+        entity.components.insert(Component::Direction, direction);
+
+        let mut acceleration: Vector4<f32> = Vector4::zero();
+        acceleration.x += cgmath::sin(cgmath::deg(dir));
+        acceleration.y += -cgmath::cos(cgmath::deg(dir));
+        acceleration = acceleration * 150.0;
+        
+        let velocity = state.add_velocity(acceleration);
+        entity.components.insert(Component::Velocity, velocity);
+
+        entity
+    }
+
+    pub fn update(&self, state: &mut EntityState, t: f32) {
+        let mut zero = Vector4::zero();
+        let acceleration = match self.components.get(&Component::Acceleration) {
+            Some(&i) => &mut state.accelerations[i],
+            None => &mut zero,
+        };
+        *acceleration = *acceleration * 1000.0;
+        let ref mut position = state.positions[*self.components.get(&Component::Position).unwrap()];
+        let ref mut velocity = state.velocities[*self.components.get(&Component::Velocity).unwrap()];
+        *position = *acceleration * t * t * 0.5f32 + *velocity * t + *position;
+        *velocity = *acceleration * t + *velocity;
+
+        if position.x < 0.0 {
+            position.x = 800.0;
+        } else if position.x > 800.0 {
+            position.x = 0.0;
         }
 
-        if self.position.y < 0.0 {
-            self.position.y = 600.0;
-        } else if self.position.y > 600.0 {
-            self.position.y = 0.0;
+        if position.y < 0.0 {
+            position.y = 600.0;
+        } else if position.y > 600.0 {
+            position.y = 0.0;
         }
 
-        self.acceleration = Vector4::zero();
+        *acceleration = Vector4::zero();
+    }
+}
+
+// FIXME: Indices might be wrong after removing one or more values
+pub struct EntityState {
+    entity_count: u32,
+    pub accelerations: Vec<Vector4<f32>>,
+    pub positions: Vec<Vector4<f32>>,
+    pub velocities: Vec<Vector4<f32>>,
+    pub directions: Vec<f32>,
+}
+
+impl EntityState {
+    pub fn new() -> EntityState {
+        EntityState {
+            entity_count: 0,
+            accelerations: Vec::new(),
+            positions: Vec::new(),
+            velocities: Vec::new(),
+            directions: Vec::new(),
+        }
+    }
+
+    fn next_id(&mut self) -> u32 {
+        let id = self.entity_count;
+        self.entity_count += 1;
+        id
+    }
+
+    fn add_acceleration(&mut self, acceleration: Vector4<f32>) -> usize {
+        let index = self.accelerations.len();
+        self.accelerations.push(acceleration);
+        index
+    }
+
+    fn add_position(&mut self, position: Vector4<f32>) -> usize {
+        let index = self.positions.len();
+        self.positions.push(position);
+        index
+    }
+
+    fn add_direction(&mut self, direction: f32) -> usize {
+        let index = self.directions.len();
+        self.directions.push(direction);
+        index
+    }
+
+    fn add_velocity(&mut self, velocity: Vector4<f32>) -> usize {
+        let index = self.velocities.len();
+        self.velocities.push(velocity);
+        index
     }
 }
